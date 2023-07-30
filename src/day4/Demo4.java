bb0511e227f0f6acb74b81288ecceba4f835d363
hayde <hayde@10.0.0.28>
2023-07-29 23:49:29 -0400

    n

--- src/day4/Demo4.java
+++ src/day4/Demo4.java
@@ -5,7 +5,11 @@ public class Demo4 {
 	public static void main(String[] args) {
 		System.out.println("Test4...");
 	}
-public static void getData() {
+
+	public static void getData() {
+
+	}
+	public static void getINfo() {
         
 	}
 }
