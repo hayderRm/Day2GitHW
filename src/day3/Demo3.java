6f1be7fca85a8b5c445e28002f9f5da86556dd1b
hayde <hayde@10.0.0.28>
2023-07-29 23:10:09 -0400

    new

--- src/day3/Demo3.java
+++ src/day3/Demo3.java
@@ -3,12 +3,11 @@ package day3;
 public class Demo3 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		System.out.println("Today is 30 Degree...");
 	}
 
-	public void getData() {
-	System.out.println("Data...");
-}
-
+	public void getINfo() {
+		System.out.println("INFO...");
+	}
 }
